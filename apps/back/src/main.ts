4e52d26e353024c1907ddf442ba2ae0186f12d14
Draggodeidad <antonioimanol7@gmail.com>
2026-06-13 02:14:22 -0600

    chore: create backend base architecture

--- apps/back/src/main.ts
+++ apps/back/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "./app.module";
-import { SWAGGER_PATH, setupSwagger } from "./common/swagger/swagger.config";
+import { SWAGGER_PATH, setupSwagger } from "./docs/swagger.config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
